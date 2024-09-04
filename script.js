4e88ee8fb8c4a6b5023b67cfe624cb53cd522d16
pankaj <pdewhare11@gmail.com>
2024-09-04 12:45:03 +0530

    4th Sept

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 console.log('Hello')
-console.log('Hi Guys')
-console.log('Hi Guys')
-console.log('Hi')
-console.log('Hello World')
+console.log('Hello1')
+console.log('Hello2')
+console.log('Hello3')
+console.log('Hello4')
+console.log('Hello5')
+console.log('Hello6')
+
